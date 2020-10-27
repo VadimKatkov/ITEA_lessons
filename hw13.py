4d7518d3231c4315173fe4cf3fb74be2c13c86ed
VadimKatkov <62391897+VadimKatkov@users.noreply.github.com>
2020-10-27 18:19:24 +0300

    first commit. Lesson6 homework file hw13.py added

---
+++ hw13.py
@@ -0,0 +1,36 @@
+'''
+Задана матрица неотрицательных чисел.
+Посчитать сумму элементов в каждом столбце. Определить, какой столбец содержит максимальную
+сумму.
+'''
+
+def matrixprint(matrix):
+    for line in matrix:
+        for j in range(len(line)):
+            print('%3d' % (line[j]), end=' ')
+        print()
+
+
+if __name__ == '__main__':
+    with open('matrix.txt') as dataFile: #читаем файл и формируем матрицу данных
+        matrix = []
+        for line in dataFile:
+            strTemp = []
+            line_new = line.strip('\n').split(' ')
+            for j in line_new:
+                strTemp.append(int(j))
+            matrix.append(strTemp)
+
+    sumArr = []
+    for line in range(len(matrix)):   # формируем список с суммами колонок
+        checkSum = 0
+        for j in range(len(matrix)):
+            checkSum += matrix[j][line]
+        sumArr.append(checkSum)
+    print()
+
+    matrixprint(matrix)  # выводим все красиво на печать
+    print (' -- '*10)
+    for i in sumArr: print(i, end=' ')
+    print()
+    print('колонка с MAX суммой:',sumArr.index(max(sumArr))+1)
